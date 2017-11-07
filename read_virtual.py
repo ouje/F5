5f58cdba930af7c0e0c294fdbe288c93c28b631c
ouje <ouje@users.noreply.github.com>
2017-11-07 16:04:32 +0100

    Update read_virtual.py

--- read_virtual.py
+++ read_virtual.py
@@ -3,28 +3,35 @@ import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 # File
-f = open('text.txt','w')
+f = open('test.txt','w')
 
 # Connect to the BigIP
 IP = "x.x.x.x"
-mgmt = ManagementRoot(IP, "username", "password")
+mgmt = ManagementRoot(IP, "XXXXX", "XXXXXXXXX")
 
-virtuals = mgmt.tm.ltm.virtuals.get_collection()
 pools = mgmt.tm.ltm.pools.get_collection()
 
-# RUN
-for NUMBER, virtual in enumerate(virtuals):
+virt = mgmt.tm.ltm.virtuals.get_collection()
+for virtual in virt:
     vip_address = virtual.destination
     vip_name = virtual.name
-     
     print >> f,('VIP: {0} {1}'.format(vip_address, vip_name))
-
-    for NUMBR, pool in enumerate(pools):
-        if NUMBR == NUMBER:
-            print >> f,('Pool: {0}'.format(pool.name))
-            for NUM, member in enumerate(pool.members_s.get_collection()):
-                address = member.address
-                name = member.name
-                print >>f, ('Node: {0} {1}'.format(address, name))
-    print >> f, ","           
-f.close() 
+    print ('VIP: {0} {1}'.format(vip_address, vip_name))
+    try:
+        vip_pool = virtual.pool
+        for pool in pools:
+             if str("/Common/" + pool.name) == str(vip_pool):
+                print ('Pool: {0}'.format(pool.name))
+                print >> f,('Pool: {0}'.format(pool.name))
+                for NUM, member in enumerate(pool.members_s.get_collection()):
+                    address = member.address
+                    name = member.name
+                    print ('Node: {0} {1}'.format(address, name))
+                    print >> f, ('Node: {0} {1}'.format(address, name))
+        print >> f, ","
+        print ","
+    except Exception:
+            continue
+            
+f.close()     
+        
